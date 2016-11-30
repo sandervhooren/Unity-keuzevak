8b796645359434c4be38af20bdeec07e8bc1ebef
sandervhooren <sandervhooren@hotmail.com>
2016-11-30 11:40:46 +0100

    Menu toegevoegd
    Menu erbij en de game met fps controller gemaakt

---
+++ Project unity/Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine; 
+using UnityEngine.UI; 
+using System.Collections; 
+
+public class Menu : MonoBehaviour 
+{ 
+	public Canvas MainCanvas; 
+	public Canvas OptionsCanvas; 
+
+	void Awake() 
+	{ 
+		OptionsCanvas.enabled = false; 
+	} 
+
+	public void OptionsOn() 
+	{
+		OptionsCanvas.enabled = true;
+		MainCanvas.enabled = false;
+	} 
+
+	public void ReturnOn() 
+	{
+		OptionsCanvas.enabled = false; 
+		MainCanvas.enabled = true; 
+	}
+
+	public void LoadOn() 
+	{ 
+		Application.LoadLevel (1);
+	} 
+}﻿
