ca21b3263aa1211f7e400ce520d93e086e0c5d64
sandervhooren <sandervhooren@hotmail.com>
2017-01-18 09:20:13 +0100

    Afgemaakt
    Fotos toegevoegd, laatste dingen gescaled en alles afgerond

---
+++ Project unity/Assets/Standard Assets/Vehicles/Drive.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+var speed : float = 10.0;
+var rotationSpeed : float = 100.0;
+function Update () {
+    var translation : float = Input.GetAxis ("Vertical") * speed;
+    var rotation : float = Input.GetAxis ("Horizontal") * rotationSpeed;
+    translation *= Time.deltaTime;
+    rotation *= Time.deltaTime;
+    transform.Translate (0, 0, translation);
+    transform.Rotate (0, rotation, 0);
+}
